e09cc262dd190709c5c721cef4616dad2c18ab72
LeelaKrishnaBoddapati <leelakrishnaboddapati6420@gmail.com>
2021-10-23 09:27:13 +0530

    Resolved errors

---
+++ MSDB/src/test/java/com/epam/hackathon/testScripts/SampleTest.java
@@ -0,0 +1,11 @@
+package com.epam.hackathon.testScripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test(alwaysRun = true)
+    public void test(){
+        System.out.println("hello");
+    }
+}
