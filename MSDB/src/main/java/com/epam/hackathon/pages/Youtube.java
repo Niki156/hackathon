7317a62c260f5343950400bdacf78a0d73885c85
Nikhil Duggi <dugginikhil123@gmail.com>
2021-10-23 12:14:39 +0530

    till sorting done

--- MSDB/src/main/java/com/epam/hackathon/pages/Youtube.java
+++ MSDB/src/main/java/com/epam/hackathon/pages/Youtube.java
@@ -1,6 +1,7 @@
 package com.epam.hackathon.pages;
 
 import com.epam.framework.base.TestBase;
+import com.epam.framework.enums.DriverType;
 import com.epam.framework.utils.WaitUtilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,18 +15,37 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class Youtube extends TestBase {
-    @FindBy(css = "div#search-container")
+
+
+    @FindBy( css = "div#icon-label")
+    WebElement sort;
+
+    @FindBy(xpath = "//input[@id='search']")
     private WebElement searchInput;
 
     @FindBy(css = "button#search-icon-legacy")
     private WebElement searchButton;
 
-    @FindBy(css = "div#container div#text-container yt-formatted-string#text")
-    private List<WebElement> listOfChannelNames;
+    @FindBy(xpath = "//*[@id=\"tabsContent\"]/tp-yt-paper-tab[2]/div")
+    private WebElement videosButton;
+
+    @FindBy(xpath = "//*[@id=\"menu\"]/a[3]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]")
+    private WebElement latestSortOption;
+
+    @FindBy(css = "div#items ytd-grid-video-renderer")
+    private List<WebElement> videos;
+
+    private By contents = By.cssSelector("div#container div#text-container yt-formatted-string#text");
 
     public WebElement getSearchInputElement(){
         return searchInput;
     }
+
+    public void launchBrowser(String url, String searchTerm, DriverType driver){
+        String finalUrl = url+"results?search_query="+searchTerm.replace(" ", "+");
+        super.launchBrowser(finalUrl, driver);
+    }
+
 
     public WebElement getSearchButtonElement(){
         WaitUtilities.waitForElementToBeVisible(driver, 10, searchButton);
@@ -34,16 +54,29 @@ public class Youtube extends TestBase {
 
 
     public void search(String searchTerm){
-        WaitUtilities.waitForElementToBeClickable(driver, 10, driver.findElement(By.id("search")));
-        driver.findElement(By.id("search")).sendKeys("Happiness");
+
         getSearchInputElement().click();
         getSearchInputElement().sendKeys(searchTerm);
         getSearchButtonElement().click();
     }
 
     public void chooseFirstElement(){
-        WaitUtilities.waitForElementToBeVisible(driver, 10, listOfChannelNames.get(0));
-        listOfChannelNames.get(0).click();
+        WaitUtilities.threadWait(5000);
+        List<WebElement> content = driver.findElements(contents);
+        content.get(0).click();
+    }
+
+    public void navigateTOVideos(){
+        String url = driver.getCurrentUrl();
+        driver.get(url+"/videos");
+    }
+
+    public void sortUsingLatest(){
+        driver.get(driver.getCurrentUrl()+"/?view=0&sort=dd&flow=grid");
+    }
+
+    public void videos(){
+        WaitUtilities.threadWait(5000);
     }
 
 
